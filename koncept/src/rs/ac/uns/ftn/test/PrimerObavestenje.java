b7ac58c51b9564587842d5fdcd55ce7b0fcf4d0e
zdugi <zdravko.dugi@gmail.com>
2020-12-07 16:07:06 +0100

    primeri za obavestenej i zahtev zavrseni

--- koncept/src/rs/ac/uns/ftn/test/PrimerObavestenje.java
+++ koncept/src/rs/ac/uns/ftn/test/PrimerObavestenje.java
@@ -26,78 +26,88 @@ import rs.ac.uns.ftn.xml_opste.TZakon;
 
 public class PrimerObavestenje {
 	public static void main(String[] argv) throws Exception {
-		//todo: read and show real document
-				System.out.println("\n\n\n ======== LOAD AND SHOW REAL DOCUMENT ======= \n\n\n");
-				
-				
-				// CREATE NEW
-				System.out.println("\n\n\n ======== CREATE NEW VALIDATE ======= \n\n\n");
-				ObjectFactory factoryObavestenje = new ObjectFactory();
-				
-				Obavestenje novoObavestenje = factoryObavestenje.createObavestenje();
-				
-				novoObavestenje.setBrojPredmeta("123-32123");
-				TDatum datum = new TDatum();
-				datum.setDatum("12.12.2020");
-				novoObavestenje.setDatum(datum);
-				TDostavljeno dostavljeno = new TDostavljeno();
-				dostavljeno.setArhivi(false);
-				dostavljeno.setImenovanom(true);
-				novoObavestenje.setDostavljeno(dostavljeno);
-				novoObavestenje.setNaziv("Naziv obavestenja");
-				TOrgan organ = new TOrgan();
-				TAdresa adresa = new TAdresa();
-				adresa.setBroj(BigInteger.valueOf(1));
-				adresa.setDrzava("DrzavaA");
-				adresa.setMesto("Mesto");
-				adresa.setPostanskiBroj(BigInteger.valueOf(1));
-				adresa.setUlica("ulica");
-				organ.setAdresa(adresa);
-				organ.setNaziv("Naziv organa");
-				novoObavestenje.setOrgan(organ);
-				TPodnosilac podnosilac = new TPodnosilac();
-				podnosilac.setAdresa(adresa);
-				podnosilac.setNaziv("naziv firme koja podnosi");
-				novoObavestenje.setPodaciPodnosioca(podnosilac);
-				TTeloObavestenja telo = new TTeloObavestenja();
-				
-				telo.getContent().add("uvodni tekst");
-				TZakon zakon = new TZakon();
-				zakon.setNazivZakona("MAGNUM KARTA LIBERTATUM");
-				telo.getContent().add(new JAXBElement<TZakon>(new QName("http://ftn.uns.ac.rs/xml_obavestenja", "Zakon"), TZakon.class, zakon));
-				telo.getContent().add("tekst kojice stojati izmedju");
-				telo.getContent().add(new JAXBElement<Integer>(new QName("http://ftn.uns.ac.rs/xml_obavestenja", "Godina"), Integer.class, 2020));
-				//todo: promeniti mozda u datum, dan.mesec?
-				telo.getContent().add(new JAXBElement<Integer>(new QName("http://ftn.uns.ac.rs/xml_obavestenja", "Dan"), Integer.class, 31));
-				telo.getContent().add(new JAXBElement<Integer>(new QName("http://ftn.uns.ac.rs/xml_obavestenja", "Pocetni_sat"), Integer.class, 8));
-				telo.getContent().add("traje do");
-				telo.getContent().add(new JAXBElement<Integer>(new QName("http://ftn.uns.ac.rs/xml_obavestenja", "Zavrsni_sat"), Integer.class, 12));
-				telo.getContent().add(
-						new JAXBElement<TAdresa>(new QName("http://ftn.uns.ac.rs/xml_obavestenja", "Adresa"), TAdresa.class, adresa));
-				telo.getContent().add(new JAXBElement<Integer>(new QName("http://ftn.uns.ac.rs/xml_obavestenja", "Kancelarija"), Integer.class, 12));
-				telo.getContent().add(new JAXBElement<Double>(new QName("http://ftn.uns.ac.rs/xml_obavestenja", "Suma"), Double.class, 321.21));
-				
-				
-				novoObavestenje.setTeloObavestenja(telo);
-				
-				JAXBContext context = JAXBContext.newInstance("rs.ac.uns.ftn.xml_obavestenja");
-				
-				Marshaller marshaller = context.createMarshaller();
+		JAXBContext context = JAXBContext.newInstance("rs.ac.uns.ftn.xml_obavestenja");
+		Unmarshaller unmarshaller = context.createUnmarshaller();
 
-				marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-				
-				marshaller.marshal(novoObavestenje, new FileOutputStream("data\\xml\\obavestenje_1.xml"));
-				
-				Unmarshaller unmarshaller = context.createUnmarshaller();
-				
-				SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-				Schema schema = schemaFactory.newSchema(new File("data\\obavestenje.xsd"));
-			
-				unmarshaller.setSchema(schema);
-		        unmarshaller.setEventHandler(new MyValidationEventHandler());
-		        
-		        Obavestenje blackmagic = (Obavestenje) unmarshaller.unmarshal(new File("data\\xml\\obavestenje_1.xml"));
-				
-				marshaller.marshal(blackmagic, System.out);
+		SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+		Schema schema = schemaFactory.newSchema(new File("data\\obavestenje.xsd"));
+
+		unmarshaller.setSchema(schema);
+		unmarshaller.setEventHandler(new MyValidationEventHandler());
+
+		Marshaller marshaller = context.createMarshaller();
+
+		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+
+		System.out.println("\n\n\n ======== LOAD AND SHOW REAL DOCUMENT ======= \n\n\n");
+
+		Obavestenje blackmagic = (Obavestenje) unmarshaller.unmarshal(new File("data\\xml\\obavestenje_original.xml"));
+
+		marshaller.marshal(blackmagic, System.out);
+
+		// CREATE NEW
+		System.out.println("\n\n\n ======== CREATE NEW VALIDATE ======= \n\n\n");
+		ObjectFactory factoryObavestenje = new ObjectFactory();
+
+		Obavestenje novoObavestenje = factoryObavestenje.createObavestenje();
+
+		novoObavestenje.setBrojPredmeta("123-32123");
+		TDatum datum = new TDatum();
+		datum.setDatum("12.12.2020");
+		novoObavestenje.setDatum(datum);
+		TDostavljeno dostavljeno = new TDostavljeno();
+		dostavljeno.setArhivi(false);
+		dostavljeno.setImenovanom(true);
+		novoObavestenje.setDostavljeno(dostavljeno);
+		novoObavestenje.setNaziv("Naziv obavestenja");
+		TOrgan organ = new TOrgan();
+		TAdresa adresa = new TAdresa();
+		adresa.setBroj(BigInteger.valueOf(1));
+		adresa.setDrzava("DrzavaA");
+		adresa.setMesto("Mesto");
+		adresa.setPostanskiBroj(BigInteger.valueOf(1));
+		adresa.setUlica("ulica");
+		organ.setAdresa(adresa);
+		organ.setNaziv("Naziv organa");
+		novoObavestenje.setOrgan(organ);
+		TPodnosilac podnosilac = new TPodnosilac();
+		podnosilac.setAdresa(adresa);
+		podnosilac.setNaziv("naziv firme koja podnosi");
+		novoObavestenje.setPodaciPodnosioca(podnosilac);
+		TTeloObavestenja telo = new TTeloObavestenja();
+
+		telo.getContent().add("uvodni tekst");
+		TZakon zakon = new TZakon();
+		zakon.setNazivZakona("MAGNUM KARTA LIBERTATUM");
+		telo.getContent().add(new JAXBElement<TZakon>(new QName("http://ftn.uns.ac.rs/xml_obavestenja", "Zakon"),
+				TZakon.class, zakon));
+		telo.getContent().add("tekst kojice stojati izmedju");
+		telo.getContent().add(new JAXBElement<Integer>(new QName("http://ftn.uns.ac.rs/xml_obavestenja", "Godina"),
+				Integer.class, 2020));
+		telo.getContent().add(
+				new JAXBElement<String>(new QName("http://ftn.uns.ac.rs/xml_obavestenja", "Opis"), String.class, ""));
+		telo.getContent().add(
+				new JAXBElement<Integer>(new QName("http://ftn.uns.ac.rs/xml_obavestenja", "Dan"), Integer.class, 31));
+		telo.getContent().add(
+				new JAXBElement<Integer>(new QName("http://ftn.uns.ac.rs/xml_obavestenja", "Sati"), Integer.class, 9));
+		telo.getContent().add(new JAXBElement<Integer>(new QName("http://ftn.uns.ac.rs/xml_obavestenja", "Pocetni_sat"),
+				Integer.class, 8));
+		telo.getContent().add("traje do");
+		telo.getContent().add(new JAXBElement<Integer>(new QName("http://ftn.uns.ac.rs/xml_obavestenja", "Zavrsni_sat"),
+				Integer.class, 12));
+		telo.getContent().add(new JAXBElement<TAdresa>(new QName("http://ftn.uns.ac.rs/xml_obavestenja", "Adresa"),
+				TAdresa.class, adresa));
+		telo.getContent().add(new JAXBElement<Integer>(new QName("http://ftn.uns.ac.rs/xml_obavestenja", "Kancelarija"),
+				Integer.class, 12));
+		telo.getContent().add(new JAXBElement<Double>(new QName("http://ftn.uns.ac.rs/xml_obavestenja", "Suma"),
+				Double.class, 321.21));
+
+		novoObavestenje.setTeloObavestenja(telo);
+
+		marshaller.marshal(novoObavestenje, new FileOutputStream("data\\xml\\obavestenje_1.xml"));
+
+		blackmagic = (Obavestenje) unmarshaller.unmarshal(new File("data\\xml\\obavestenje_1.xml"));
+
+		marshaller.marshal(blackmagic, System.out);
 	}
 }
--- koncept/src/rs/ac/uns/ftn/test/PrimerZahtev.java
+++ koncept/src/rs/ac/uns/ftn/test/PrimerZahtev.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.math.BigInteger;
-import java.util.GregorianCalendar;
 
 import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
@@ -13,7 +12,6 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.datatype.DatatypeConfigurationException;
-import javax.xml.datatype.DatatypeFactory;
 import javax.xml.namespace.QName;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
@@ -27,6 +25,7 @@ import rs.ac.uns.ftn.xml_opste.TDodatneInformacije;
 import rs.ac.uns.ftn.xml_opste.TOrgan;
 import rs.ac.uns.ftn.xml_opste.TOsoba;
 import rs.ac.uns.ftn.xml_opste.TTrazilac;
+import rs.ac.uns.ftn.xml_zahtev.TInformacije;
 import rs.ac.uns.ftn.xml_zahtev.TParagraf;
 import rs.ac.uns.ftn.xml_zahtev.TTeloZahteva;
 import rs.ac.uns.ftn.xml_zahtev.TTipDostave;
@@ -38,26 +37,26 @@ import rs.ac.uns.ftn.xml_zahtev.Zahtev;
 public class PrimerZahtev {
 
 	public static void main(String[] args) throws JAXBException, DatatypeConfigurationException, FileNotFoundException, SAXException {
+		JAXBContext context = JAXBContext.newInstance("rs.ac.uns.ftn.xml_zahtev");
+		Marshaller marshaller = context.createMarshaller();
+		Unmarshaller unmarshaller = context.createUnmarshaller();
+
+		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+		
+		SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+		Schema schema = schemaFactory.newSchema(new File("data\\zahtev.xsd"));
+        
+		// Podešavanje unmarshaller-a za XML schema validaciju
+		unmarshaller.setSchema(schema);
+        unmarshaller.setEventHandler(new MyValidationEventHandler());
+		
 		//todo: read and show real document
 		System.out.println("\n\n\n ======== LOAD AND SHOW REAL DOCUMENT ======= \n\n\n");
-		JAXBContext context = JAXBContext.newInstance("rs.ac.uns.ftn.xml_zahtev");
 		
-		Unmarshaller unmarshaller = context.createUnmarshaller();
-		Zahtev zahtev = (Zahtev) unmarshaller.unmarshal(new File("data\\xml\\zahtev1.xml"));
+		Zahtev zahtev = (Zahtev) unmarshaller.unmarshal(new File("data\\xml\\zahtev_original.xml"));
 		
-		/*
-		JAXBElement<?> ser = (JAXBElement<?>)zahtev.getTeloZahteva().getParagraf().getContent().get(1);
-		TTipoviZahteva ttip = (TTipoviZahteva)ser.getValue();
+		marshaller.marshal(zahtev, System.out);
 		
-		System.out.println(ttip.getTipZahteva().get(1).getContent().get(0));
-		*/
-		/*
-		JAXBElement<TTipoviZahteva> ser = (JAXBElement<TTipoviZahteva>)zahtev.getTeloZahteva().getParagraf().getContent().get(1);
-		System.out.println(ser.getValue().getClass());
-		
-		System.out.println(zahtev);*/
-		
-		// Instanciranje Studenti klase posredstvom ObjectFactory-a
 		// CREATE NEW
 		System.out.println("\n\n\n ======== CREATE NEW & VALIDATE ======= \n\n\n");
 		rs.ac.uns.ftn.xml_zahtev.ObjectFactory factoryZahtev = new rs.ac.uns.ftn.xml_zahtev.ObjectFactory();
@@ -109,7 +108,7 @@ public class PrimerZahtev {
 		
 		tipoviDostave.getTipDostave().add(dostava1);
 		
-		JAXBElement<TTipoviDostave> jaxbTTipovi = new JAXBElement(new QName("http://ftn.uns.ac.rs/xml_zahtev", "Tipovi_dostave"), TTipoviDostave.class, tipoviDostave);
+		JAXBElement<TTipoviDostave> jaxbTTipovi = new JAXBElement<TTipoviDostave>(new QName("http://ftn.uns.ac.rs/xml_zahtev", "Tipovi_dostave"), TTipoviDostave.class, tipoviDostave);
 		
 		tip3.getContent().add(jaxbTTipovi);
 		
@@ -118,10 +117,17 @@ public class PrimerZahtev {
 		tipovi.getTipZahteva().add(tip3);
 		
 		paragraf.getContent().add("Tekst paragrafa\n");
-		JAXBElement<TTipoviZahteva> jaxbTipovi = new JAXBElement(new QName("http://ftn.uns.ac.rs/xml_zahtev", "Tipovi_zahteva"), TTipoviZahteva.class, tipovi);
+		JAXBElement<TTipoviZahteva> jaxbTipovi = new JAXBElement<TTipoviZahteva>(new QName("http://ftn.uns.ac.rs/xml_zahtev", "Tipovi_zahteva"), TTipoviZahteva.class, tipovi);
 		paragraf.getContent().add(jaxbTipovi);
 		
 		telo.setParagraf(paragraf);
+		
+		TInformacije informacije = new TInformacije();
+		informacije.setNaslov("Naslov");
+		informacije.setOpis("Smislen opis");
+		informacije.setSavet("Hint");
+		
+		telo.setInformacije(informacije);
 		
 		// set telo
 		noviZahtev.setTeloZahteva(telo);
@@ -150,29 +156,12 @@ public class PrimerZahtev {
 		// set dodatne
 		noviZahtev.setDodatneInformacije(dodatne);
 		
-		
-		// Generiše se novi student
-		// studenti.getStudent().add(createStudent(12345, "Tijana", "Novkovic"));
-				
-		
-	
-		Marshaller marshaller = context.createMarshaller();
 
-		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-		
 		marshaller.marshal(noviZahtev, new FileOutputStream("data\\xml\\magic.xml"));
-	
-		
-		SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-		Schema schema = schemaFactory.newSchema(new File("data\\zahtev.xsd"));
-        
-		// Podešavanje unmarshaller-a za XML schema validaciju
-		unmarshaller.setSchema(schema);
-        unmarshaller.setEventHandler(new MyValidationEventHandler());
 		
 		Zahtev blackmagic = (Zahtev) unmarshaller.unmarshal(new File("data\\xml\\magic.xml"));
 		
-		System.out.println(blackmagic);
+		// System.out.println(blackmagic);
 		marshaller.marshal(blackmagic, System.out);
 	}
 
