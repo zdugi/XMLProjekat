1dcd81db2864f92c2fd1359b5681dd8a7193164b
zdugi <zdravko.dugi@gmail.com>
2021-01-10 10:44:14 +0100

    init project_poverenik

---
+++ project_poverenik/src/main/java/com/xmlproject/project_poverenik/ProjectPoverenikApplication.java
@@ -0,0 +1,13 @@
+package com.xmlproject.project_poverenik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPoverenikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPoverenikApplication.class, args);
+	}
+
+}
---
+++ project_poverenik/src/test/java/com/xmlproject/project_poverenik/ProjectPoverenikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xmlproject.project_poverenik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPoverenikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
