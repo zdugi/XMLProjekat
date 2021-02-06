3ed529530b56472409e84e1840e8809b640cdaab
vsmiljana <smiljana.vojvodic@gmail.com>
2021-02-06 04:01:44 +0100

    css

--- project_poverenik/src/main/resources/static/js/menu.js
+++ project_poverenik/src/main/resources/static/js/menu.js
@@ -6,22 +6,41 @@ Vue.component('menu-component', {
         }
     },
     template: `
-    <div>
+    <nav role="navigation">
     <ul v-if="checkUser()==='ROLE_POVERENIK'">
-        <li><router-link to="/requests-table-page">Listanje zalbi na cutanje</router-link></li>
-         <li><router-link to='/complaint-res-list'>Listanje zalbi na odluku</router-link></li>
-         <li><router-link to='/resolution-list'>Listanje resenja</router-link></li>
-          <li><router-link to="/reports-list">Listanje izvestaja</router-link></li>
-         <li><router-link to='/create-resolution'>Kreiraj resenje</router-link></li>
-        <li><router-link to="/search">Pretraga zalbi na cutanje</router-link></li>
-         <li><router-link to="/search-complaint-res">Pretraga zalbi na odluku</router-link></li>
-         <li><router-link to='/resolution-search'>Pretraga resenja</router-link></li>
-         <li><router-link to="/reports-search">Pretraga izvestaja</router-link></li>
-        <li><router-link to="/advance-search">Napredna pretraga zalbi na cutanje</router-link></li>
-        <li><router-link to="/advance-search-complaint-res">Napredna pretraga zalbi na odluku</router-link></li>
-         <li><router-link to="/advance-search-resolution">Napredna pretraga resenja</router-link></li>
-         <li><router-link to="/reports-advance-search">Napredna pretraga izvestaja</router-link></li>
-        <li><router-link to="/messenger">Poruke</router-link></li>
+        <li><router-link to="/requests-table-page">Zalbe na cutanje</router-link>
+            <ul class="dropdown">
+               <li><router-link to="/requests-table-page">Listanje zalbi na cutanje</router-link></li>
+                <li><router-link to="/search">Pretraga zalbi na cutanje</router-link></li>
+              <li><router-link to="/advance-search">Napredna pretraga zalbi na cutanje</router-link></li>
+
+            </ul>
+        </li>
+        <li><router-link to="/complaint-res-list">Zalbe na odluku</router-link>
+           <ul class="dropdown">
+             <li><router-link to='/complaint-res-list'>Listanje zalbi na odluku</router-link></li>
+              <li><router-link to="/search-complaint-res">Pretraga zalbi na odluku</router-link></li>
+              <li><router-link to="/advance-search-complaint-res">Napredna pretraga zalbi na odluku</router-link></li>
+            </ul>
+        </li>
+
+        <li><router-link to="/reports-list">Izvestaji</router-link>
+           <ul class="dropdown">
+                <li><router-link to="/reports-list">Listanje izvestaja</router-link></li>
+                 <li><router-link to="/reports-search">Pretraga izvestaja</router-link></li>
+                <li><router-link to="/reports-advance-search">Napredna pretraga izvestaja</router-link></li>
+            </ul>
+        </li>
+
+        <li><router-link to="/resolution-list">Resenja</router-link>
+            <ul class="dropdown">
+                <li><router-link to='/create-resolution'>Kreiraj resenje</router-link></li>
+                <li><router-link to='/resolution-list'>Listanje resenja</router-link></li>
+                <li><router-link to='/resolution-search'>Pretraga resenja</router-link></li>
+                <li><router-link to="/advance-search-resolution">Napredna pretraga resenja</router-link></li>
+            </ul>
+         </li>
+         <li><router-link to="/messenger">Poruke</router-link></li>
 
          <li><span style="color: #9b4dca;text-decoration: none;cursor: pointer;" v-on:click="logout()">Odjavi se</span></li>
 
@@ -39,7 +58,7 @@ Vue.component('menu-component', {
              <li><span style="color: #9b4dca;text-decoration: none;cursor: pointer;" v-on:click="logout()">Odjavi se</span></li>
 
         </ul>
-    </div>
+    </nav>
     `,
          methods: {
                  logout() {
